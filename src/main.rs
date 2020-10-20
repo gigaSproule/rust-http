47002a89b621f3df79d96fdf549d4925e819d6a3
Benjamin Sproule <benjamin.sproule@bmigroup.com>
2020-10-20 23:53:36 +0100

    Added new endpoint

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,18 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::http::RawStr;
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/<name>")]
+fn hello(name: &RawStr) -> String {
+    format!("Hello, {}!", name.as_str())
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, hello]).launch();
 }
